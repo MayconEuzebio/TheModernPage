cd82ee7846fdc8a80235123d236986894d574d9e
MayconEuzebio <euzebio.maycon@gmail.com>
2022-03-23 10:54:39 -0300

    New input interaction

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,25 @@ function Home() {
     )
 }
 
+function daysBetween(date1, date2) {
+
+    console.log(date1, date2);
+    // The number of milliseconds in one day
+    const ONE_DAY = 1000 * 60 * 60 * 24;
+
+    // Calculate the difference in milliseconds
+    const differenceMs = Math.abs(date1 - date2);
+
+    // Convert back to days and return
+    return Math.round(differenceMs / ONE_DAY);
+
+}
+
 function CountLovesDay() {
-    const   days = 74,
-            [count, setCountLove] = useState(days);
+    const   days = daysBetween(new Date('01/07/2022'), new Date()),
+            [   count,
+                weddingDay, 
+                setCountLove,] = useState(days);
 
     function countDays() {
         setCountLove(count + 1);
@@ -22,11 +38,27 @@ function CountLovesDay() {
         setCountLove(days);
     }
 
+    function inputWeddingDay() {
+        if ( Object.prototype.toString.call(weddingDay) === '[object Date]' )
+        //if ( false === true)
+            return false
+        else {
+            return (
+                <div>
+                    <label>Inform the wedding day</label>
+                    <input type="date" id="weddingDay" name="weddingDay" value={weddingDay}></input>
+                    <br></br>
+                </div>
+            )
+        }
+    }
+
     return (
         <div>
             <br></br>
             <div>{count} days of marriage</div>
             <br></br>
+            {inputWeddingDay()}
             <button onClick={countDays}>And the other day?</button>
             <button onClick={clear}>Today?</button>
         </div>
